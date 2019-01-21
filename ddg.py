a279642f86593d21c29fde89dab521b468cc6ec6
carlos <cquintanamatoses@gmail.com>
2019-01-21 16:02:46 +0100

    Mi commit

---
+++ ddg.py
@@ -0,0 +1,9 @@
+def ddg(jugador):
+    jugador = int(jugador)
+    jugadores = ["Juan", "Maria", "Carlos"]
+    for i in range(len(jugadores)):
+        if i == jugador:
+            return jugadores[i]
+
+jugador = input("Introduce numero: ")
+print(ddg(jugador))
